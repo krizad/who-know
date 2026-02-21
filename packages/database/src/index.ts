b8c5c8904182927975aef1c3ddf8a8ab707833c3
kridsada <vim_intason@hotmail.com>
2026-02-22 01:20:53 +0700

    feat: Implement Prisma Client with `@prisma/adapter-pg` for PostgreSQL connection pooling and update Prisma dependencies.

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,6 +1,12 @@
 import { PrismaClient } from '@prisma/client';
+import { Pool } from 'pg';
+import { PrismaPg } from '@prisma/adapter-pg';
 
 export * from '@prisma/client';
+
+const connectionString = `${process.env.DATABASE_URL}`;
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
@@ -9,6 +15,7 @@ const globalForPrisma = globalThis as unknown as {
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
+    adapter,
     log: ['query'],
   });
 
